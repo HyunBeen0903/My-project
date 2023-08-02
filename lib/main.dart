68c4a194d4c5c281ea24a55a511aed6939fa2882
Kim Hyun Bin <hhb0664@naver.com>
2023-08-02 23:30:04 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'json_parse.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: JsonParse(),
+     // home: TeamProject(),
     );
   }
 }
